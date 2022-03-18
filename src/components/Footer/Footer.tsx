2d229c5ca30f07b703057794df03893496fa0ed1
alphadev0101 <alphadev0101@gmail.com>
2022-03-18 17:35:30 -0300

    Cambiando footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
     return (
     <footer className={styles.footer}>
         <div className="footer-text">
-                    <p>&copy; JuppLCD - 2022</p>
+                    <p>Copyright &copy; 2022. Lorenzo Canovas All Rights Reserved</p>
                 </div>
     </footer>
     )
